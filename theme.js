1abc7dbd117fd36c8bc62d74797ca3076b2d943a
Brian Hassett <bhassett.73@gmail.com>
2019-03-14 13:57:25 -0400

    Adding content with javascript

--- theme.js
+++ theme.js
@@ -1,3 +1,12 @@
 $(document).on('hover', '.megamenu .dropdown-menu', function(e) {
     e.stopPropagation()
 });
+
+var newh1 = document.createElement("h1");
+var newp = document.createElement("p");
+
+newh1.innerHTML = "Top & Best";
+newp.innerHTML = "The very best of what you're looking for"
+
+document.getElementById('container') .appendChild(newh1);
+document.getElementById('container') .appendChild(newp);
